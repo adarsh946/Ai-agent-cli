af1f938a44aeef93d094ae781f3678963d5a4870
adarsh946 <adarsh.shukla2724@gmail.com>
2026-01-07 22:41:10 +0530

    created landing page ui

---
+++ client/lib/auth-client.ts
@@ -0,0 +1,8 @@
+import { createAuthClient } from "better-auth/react";
+import { deviceAuthorizationClient } from "better-auth/plugins";
+
+export const authClient = createAuthClient({
+  baseURL: "http://localhost:3005",
+
+  plugins: [deviceAuthorizationClient()],
+});
